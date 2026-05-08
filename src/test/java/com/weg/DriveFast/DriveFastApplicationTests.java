0246c06a1e2935539eaf983b2644b8268c76eb23
NÍCOLLAS GABRIEL BARTKO DE FRANÇA <nicollas_franca@estudante.sesisenai.org.br>
2026-05-08 18:40:27 -0300

    NF-01 - feat: inicia versionamento do projeto

---
+++ src/test/java/com/weg/DriveFast/DriveFastApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weg.DriveFast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DriveFastApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
