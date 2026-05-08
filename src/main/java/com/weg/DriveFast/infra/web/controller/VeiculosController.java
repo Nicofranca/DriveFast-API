588df657163beae9389f1a4f3c9f5cc9e8d64138
nicollas_franca <nicollas_franca@estudante.sesisenai.org.br>
2026-05-08 19:00:38 -0300

    NF-06 - feat: criação do Controller VeiculoController.java

---
+++ src/main/java/com/weg/DriveFast/infra/web/controller/VeiculosController.java
@@ -0,0 +1,9 @@
+package com.weg.DriveFast.infra.web.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/veiculos")
+public class VeiculosController {
+}
