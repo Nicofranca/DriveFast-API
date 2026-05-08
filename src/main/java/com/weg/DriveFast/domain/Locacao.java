99ebcbe91b8d8847d28d015670a59a69690f7837
nicollas_franca <nicollas_franca@estudante.sesisenai.org.br>
2026-05-08 18:56:58 -0300

    NF-04 - feat: criação da entidade Locacao.java

---
+++ src/main/java/com/weg/DriveFast/domain/Locacao.java
@@ -0,0 +1,33 @@
+package com.weg.DriveFast.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "locacao")
+public class Locacao {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Integer dias;
+    private BigDecimal valorTotal;
+
+    @ManyToOne
+    @JoinColumn(name = "cliente_id")
+    private Cliente cliente;
+
+    @ManyToOne
+    @JoinColumn(name = "veiculo_id")
+    private Veiculo veiculo;
+}
