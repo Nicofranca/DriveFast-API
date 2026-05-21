9c0f8b00280b5649c4c6b539602f6682126eb887
Nicofranca <nicollas_franca@estudante.sesisenai.org.br>
2026-05-20 21:55:05 -0300

    NF-18 - feat: Implementa metodos da interface LocacaoService

--- src/main/java/com/weg/DriveFast/application/service/ClienteServiceImpl.java
+++ src/main/java/com/weg/DriveFast/application/service/ClienteServiceImpl.java
@@ -2,8 +2,37 @@ package com.weg.DriveFast.application.service;
 
 import com.weg.DriveFast.application.dto.cliente.ClienteCreateDTO;
 import com.weg.DriveFast.application.dto.cliente.ClienteResponseDTO;
+import com.weg.DriveFast.application.dto.cliente.ClienteUpdateDTO;
+import com.weg.DriveFast.application.dto.mensagens.MensagemDTO;
+import com.weg.DriveFast.domain.service.ClienteService;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-public class ClienteServiceImpl {
+public class ClienteServiceImpl implements ClienteService {
+    @Override
+    public ClienteResponseDTO save(ClienteCreateDTO clienteCreateDTO) {
+        return null;
+    }
+
+    @Override
+    public List<ClienteResponseDTO> findAll() {
+        return List.of();
+    }
+
+    @Override
+    public ClienteResponseDTO findById(Long id) {
+        return null;
+    }
+
+    @Override
+    public ClienteResponseDTO update(Long clienteId, ClienteUpdateDTO clienteUpdateDTO) {
+        return null;
+    }
+
+    @Override
+    public MensagemDTO delete(Long id) {
+        return null;
+    }
 }
--- src/main/java/com/weg/DriveFast/application/service/LocacaoServiceImpl.java
+++ src/main/java/com/weg/DriveFast/application/service/LocacaoServiceImpl.java
@@ -1,8 +1,29 @@
 package com.weg.DriveFast.application.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
+import com.weg.DriveFast.application.dto.locacao.LocacaoResponseDTO;
+import com.weg.DriveFast.application.dto.mensagens.MensagemDTO;
+import com.weg.DriveFast.domain.service.LocacaoService;
+
 @Service
-public class LocacaoServiceImpl {
+public class LocacaoServiceImpl implements LocacaoService{
+
+    @Override
+    public LocacaoResponseDTO save(Long clienteId, Long veiculoId, int dias) {
+        throw new UnsupportedOperationException("Unimplemented method 'save'");
+    }
+
+    @Override
+    public List<LocacaoResponseDTO> findAll() {
+        throw new UnsupportedOperationException("Unimplemented method 'findAll'");
+    }
+
+    @Override
+    public MensagemDTO delete(Long id) {
+        throw new UnsupportedOperationException("Unimplemented method 'delete'");
+    }
 
 }
