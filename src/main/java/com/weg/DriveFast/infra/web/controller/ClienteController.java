bec1273d9aa408746e81da82935cb8e36e1da86e
Nicofranca <nicollas_franca@estudante.sesisenai.org.br>
2026-05-22 15:41:28 -0300

    NF-22 - feat: finalizacao do controller de Cliente

--- src/main/java/com/weg/DriveFast/infra/web/controller/ClienteController.java
+++ src/main/java/com/weg/DriveFast/infra/web/controller/ClienteController.java
@@ -4,19 +4,20 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.weg.DriveFast.application.dto.cliente.ClienteCreateDTO;
 import com.weg.DriveFast.application.dto.cliente.ClienteResponseDTO;
+import com.weg.DriveFast.application.dto.cliente.ClienteUpdateDTO;
+import com.weg.DriveFast.application.dto.mensagens.MensagemDTO;
 import com.weg.DriveFast.domain.service.ClienteService;
-
-
-
 
 @RestController
 @RequestMapping("/clientes")
@@ -46,5 +47,15 @@ public class ClienteController {
             .body(clienteService.findById(id));
     }
     
-    
+    @PutMapping("/{id}")
+    public ResponseEntity<ClienteResponseDTO> update(@PathVariable Long id, @RequestBody ClienteUpdateDTO clienteUpdateDTO){
+        return ResponseEntity.status(HttpStatus.OK)
+            .body(clienteService.update(id, clienteUpdateDTO));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<MensagemDTO> delete(@PathVariable Long id){
+        return ResponseEntity.status(HttpStatus.OK)
+            .body(clienteService.delete(id));
+    }
 }
